5051b4da74bd933a680169efb3bb06b7b04f748b
CPGRU <pei19860202@gmail.com>
2024-08-15 11:44:53 +0200

    added metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import CardWrapper from '../../ui/dashboard/cards';
 import RevenueChart from '../../ui/dashboard/revenue-chart';
 import LatestInvoices from '../../ui/dashboard/latest-invoices';
@@ -8,6 +9,10 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton
 } from '@/app/ui/skeletons';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
  
 export default async function Page() {
   
